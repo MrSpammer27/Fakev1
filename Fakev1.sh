50ff7c406c7971a546732d41c9fae4927e132c38
MrSpammer27 <82670147+MrSpammer27@users.noreply.github.com>
2021-05-19 05:56:11 +0700

    Add files via upload

---
+++ Fakev1.sh
@@ -0,0 +1,28 @@
+
+
+clear
+echo "==================================="
+echo "(+) AUTHOR:MR SPAMMER (+)
+echo "•••••••••••••••••••••••••••••••••••"
+echo "(+) SCRIPT: FAKE HACK FACEBOOK (+)"
+echo "•••••••••••••••••••••••••••••••••••"
+echo "(+) PESAN: HANYA UNTUK HIBURAN (+)"
+echo "==================================="
+echo
+echo "HACK FACEBOOK"
+echo "####################"
+echo 
+read -p "MASUKAN ID TARGET => " tur
+
+if [ Stur = 1234567 ] 
+then
+echo "(!) NAMA : ZAHRA CANS"
+echo "(+) PASWORD : ZHR8857GG"
+echo "(?) ID : 1234567"
+echo
+echo "(!) NAMA : NUDIN CS"
+echo "(+) PASWORD : ANAK KECE 123"
+echo "(+) ID : 1234567"
+echo
+echo
+fi
